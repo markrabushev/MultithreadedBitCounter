3593022d6400e691a2746cdc35b3ccc8d6bf6f88
Mark Rabushev <100849266+markrabushev@users.noreply.github.com>
2025-08-28 17:51:29 +0000

    add bits counter

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all: bit_counter
 
 bit_counter: bit_counter.c
-	gcc -o bit_counter bit_counter.c
+	gcc -o bit_counter bit_counter.c -lpthread
 
 clean:
 	rm -f bit_counter
--- bit_counter.c
+++ bit_counter.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <pthread.h>
 
 struct Node {
     int value;
@@ -9,7 +10,6 @@ struct Node {
 };
 
 struct LinkedList {
-    int size;
     struct Node *head;
     struct Node *tail;
 };
@@ -17,7 +17,6 @@ struct LinkedList {
 void list_init(struct LinkedList* list, int n) {
     list->head = NULL;
     list->tail = NULL;
-    list->size = n;
     for (int i = 0; i < n; i++) {
         struct Node* node = (struct Node*) malloc(sizeof(struct Node));
         node->value = rand();
@@ -31,6 +30,77 @@ void list_init(struct LinkedList* list, int n) {
             list->head = node;
         }
     }
+}
+
+int count_zero_bits(int value) {
+    int n = 0;
+    unsigned int u = (unsigned int)value;
+    for (int i = 0; i < 32; i++, u >>= 1) {
+        if ((u & 1) == 0) n++;
+    }
+    return n;
+}
+
+int count_one_bits(int value) {
+    int n = 0;
+    unsigned int u = (unsigned int)value;
+    for (int i = 0; i < 32; i++, u >>= 1) {
+        if (u & 1) n++;
+    }
+    return n;
+}
+
+struct thread_arg {
+    struct LinkedList* list;
+    int direction;
+};
+
+void* count_bits(void* arg) {
+    struct thread_arg* targ = (struct thread_arg*)arg;
+    struct LinkedList* list = targ->list;
+    int direction = targ->direction;
+
+    unsigned long long bit_count = 0;
+    int elements = 0;
+
+    while (1) {
+        if (list->head == NULL) {
+            break;
+        }
+
+        struct Node* node = NULL;
+        if (direction == 0) {
+            node = list->head;
+            if (list->head == list->tail) {
+                list->head = NULL;
+                list->tail = NULL;
+            } else {
+                list->head = list->head->next;
+                list->head->prev = NULL;
+            }
+        } else {
+            node = list->tail;
+            if (list->head == list->tail) {
+                list->head = NULL;
+                list->tail = NULL;
+            } else {
+                list->tail = list->tail->prev;
+                list->tail->next = NULL;
+            }
+        }
+
+        if (direction == 0) {
+            bit_count += count_zero_bits(node->value);
+        } else {
+            bit_count += count_one_bits(node->value);
+        }
+        elements++;
+        free(node);
+    }
+
+    printf("Количество учтенных элементов %d, Количество %d-х битов, начиная с %s: %llu\n",
+           elements, direction, direction == 0 ? "головы" : "хвоста", bit_count);
+    return NULL;
 }
 
 int main(int argc, char const *argv[])
@@ -47,6 +117,12 @@ int main(int argc, char const *argv[])
     srand(time(NULL));
     struct LinkedList list;
     list_init(&list, n);
+
+    pthread_t thread1;
+    struct thread_arg arg1 = { &list, 0 };
+    pthread_create(&thread1, NULL, count_bits, &arg1);
+
+    pthread_join(thread1, NULL);
 
     return 0;
 }
